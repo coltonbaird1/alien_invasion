8c9ce7dead8b42c6e356043d320e36012781dd45
coltonbaird1 <coltonbmain@gmail.com>
2024-03-03 14:27:54 -0700

    Drawing the ship to the screen

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ class Settings():
     def __init__(self):
         """Initialize games settings."""
         # Screen Settings
-        self.screen_width = 1200
-        self.screen_height = 800
+        self.screen_width = 1000
+        self.screen_height = 600
         self.bg_color = (230, 230, 230)
-        
+
