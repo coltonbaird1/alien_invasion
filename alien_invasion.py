b50e138d8b88e69f675a2ce135ae39fc0236b52a
coltonbaird1 <coltonbmain@gmail.com>
2024-03-05 12:33:21 -0700

    Refactoring: added game_functions module
    Moved function to close screen to its own module and then
    imported it into alien_invasion.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,8 +1,8 @@
-import sys
 import pygame
 
 from settings import Settings
 from ship import Ship
+import game_functions as gf
 
 def run_game():
     #initialize game and create a screen object
@@ -20,11 +20,8 @@ def run_game():
 
     # Start the main loop for the game
     while True:
+        gf.check_events()
 
-        # watch for keyboard and mouse events
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
 
         # Redraw the screen during each pass through the loop
         screen.fill(ai_settings.bg_color)
