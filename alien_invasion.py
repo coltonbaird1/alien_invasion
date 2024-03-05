bf26704a51708d0abebb3ab15cea103f42d2bf70
coltonbaird1 <coltonbmain@gmail.com>
2024-03-05 16:49:47 -0700

    Storing bullets in a group

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.sprite import Group
 
 from settings import Settings
 from ship import Ship
@@ -14,15 +15,18 @@ def run_game():
 
     # Make a ship
     ship = Ship(ai_settings, screen)
+    # Make a group to store bullets
+    bullets = Group()
 
     # set the background color
     bg_color = (230, 230, 230)
 
     # Start the main loop for the game
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        bullets.update()
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
         # Make most recently drawn screen visible
         pygame.display.flip()
